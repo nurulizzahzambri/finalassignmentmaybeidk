fa393fd5ad8042ec64f9bb4ff3667bef0bdab66f
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 19:41:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
