1597e9fc44f5b9d0b48b435f0e1569f1785ecf4d
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 18:40:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,64 @@ phone_data = pd.read_csv(url)
 
 st.write(phone_data.head(5))
 
+X = phone_data.drop(['price_range'], axis = 1)
+y = phone_data['price_range']
+
+# split data
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, 
+                                                test_size = 0.2, 
+                                                random_state = 999)
+
+
 option = st.sidebar.selectbox(
      'Select a Classification method',
       ['K-NN','SVM','Logistic Regression','Gaussian Naive Bayes','Random Forest'])
 
 if option == 'K-NN':
   st.write('This is the K-NN method')
-    
+
+  knn = KNeighborsClassifier()
+  knn.fit(Xtrain, ytrain)
+  ypred = knn.predict(Xtest)
+  
+  print(classification_report(ytest, ypred))
 
 elif option == 'SVM':
   st.write('This is the SVM method')
+
+  svc = SVC()
+  svc.fit(Xtrain, ytrain)
+  ypred = svc.predict(Xtest)
+  
+  print(classification_report(ytest, ypred))
   
 
 elif option == 'Logistic Regression':
   st.write('This is the Logistic Regression method')
+
+  logreg = LogisticRegression()
+  logreg.fit(Xtrain, ytrain)
+  ypred = logreg.predict(Xtest)
+  
+  print(classification_report(ytest, ypred))
     
 
 elif option == 'Gaussian Naive Bayes':
   st.write('This is the Gaussian Naive Bayes method')
+
+  nb = GaussianNB()
+  nb.fit(Xtrain, ytrain)
+  ypred = nb.predict(Xtest)
+  
+  print(classification_report(ytest, ypred))
   
   
 elif option == 'Random Forest':
   st.write('This is the Random Forest method')
+
+  rf = RandomForestClassifier()
+  rf.fit(Xtrain, ytrain)
+  ypred = rf.predict(Xtest)
+  
+  print(classification_report(ytest, ypred))
