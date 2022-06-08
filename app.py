f33e80a460c201d0846668c37a5ab1ed95018843
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-08 21:43:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -101,12 +101,26 @@ st.write("""
 #sns.heatmap(phone_data.corr(), cmap = "PuOr", annot = True, vmin = -1, vmax = 1, center = 0)
 #st.pyplot(fig)
 
+x = st.sidebar.selectbox(
+     'Select an X',
+      ['battery_power', 'blue', 'clock_speed', 'dual_sim', 'fc', 'four_g',
+       'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height',
+       'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time', 'three_g',
+       'touch_screen', 'wifi', 'price_range'])
+
+y = st.sidebar.selectbox(
+     'Select a y',
+      ['battery_power', 'blue', 'clock_speed', 'dual_sim', 'fc', 'four_g',
+       'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height',
+       'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time', 'three_g',
+       'touch_screen', 'wifi', 'price_range'])
+
 st.vega_lite_chart(phone_data, {
      'width': 800,
      'height': 800,
      'mark': {'type': 'circle', 'tooltip': True},
      'encoding': {
-         'x': {'field': 'ram', 'type': 'quantitative'},
-         'y': {'field': 'price_range', 'type': 'quantitative'},
+         'x': {'field': x, 'type': 'quantitative'},
+         'y': {'field': y, 'type': 'quantitative'},
      },
  })
