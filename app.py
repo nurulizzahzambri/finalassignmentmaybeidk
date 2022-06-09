b7aa35ccb8ec71018d0d53fcfbd5d304feba1e67
nurulizzahzambri <106502769+nurulizzahzambri@users.noreply.github.com>
2022-06-09 14:00:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ with col1:
 with col2:
     fig, ax = plt.subplots(figsize=(19, 15))
     ax.matshow(phone_data.corr(),cmap = 'PuOr' )
-    ax.xticks(range(phone_data.columns, fontsize=14, rotation=45)
-    ax.yticks(range(phone_data.columns, fontsize=14)
+    #plt.xticks(range(phone_data.columns, fontsize=14, rotation=45)
+    #plt.yticks(range(phone_data.columns, fontsize=14)
 
     st.pyplot(fig)
 
